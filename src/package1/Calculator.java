041657ccb86010732131470afbb7786cce1ae0d5
GabrielLacerda00 <gabriel.lacerd.gr@gmail.com>
2023-05-16 09:12:23 -0300

    commit inicial

---
+++ src/package1/Calculator.java
@@ -0,0 +1,19 @@
+package package1;
+
+public class Calculator {
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public float division(int a, int b) {
+		return a / b;
+	}
+}
